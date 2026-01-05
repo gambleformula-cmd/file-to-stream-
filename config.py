e4634bf3b1fbf038f14218646fcba210b0be64b6
gambleformula-cmd <gambleformula@gmail.com>
2026-01-05 11:37:58 +0530

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN"
-CHANNEL_USERNAME = "@yourchannelusername"
-SECRET_KEY = "randomsecret123"
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME")
+SECRET_KEY = os.environ.get("SECRET_KEY", "secret123")
+
