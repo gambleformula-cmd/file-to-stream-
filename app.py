2280ef11d2322c266ba40dbfd704860b4d73a88a
gambleformula-cmd <gambleformula@gmail.com>
2026-01-05 15:08:33 +0530

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
-from telegram import check_join
+from flask import Flask, render_template
 from config import SECRET_KEY
 import os
 
@@ -9,20 +8,6 @@ app.secret_key = SECRET_KEY
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/course")
-def course():
-    user_id = request.args.get("uid")
-    if not user_id:
-        return redirect(url_for("lock"))
-
-    if check_join(user_id):
-        return redirect("https://t.me/your_private_channel")  # VIDEO
-    return redirect(url_for("lock"))
-
-@app.route("/lock")
-def lock():
-    return render_template("lock.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
