aba98eec030b65259c047f1b80893a03982b2acc
gambleformula-cmd <gambleformula@gmail.com>
2026-01-07 09:01:49 +0530

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 import os
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "anyrandomstring")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# 🔥 CHANGE BOT HERE ONLY (or via Render ENV)
+ACTIVE_BOT_URL = os.environ.get(
+    "ACTIVE_BOT_URL",
+    "https://t.me/YourMainVideoBot"
+)
